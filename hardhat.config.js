836dc04f0d16494a87ab00f7f5bc730bbfaec5cb
m r <trash89@laposte.net>
2022-02-25 21:01:04 +0100

    config

---
+++ hardhat.config.js
@@ -0,0 +1,44 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      initialBaseFeePerGas: 0,
+    },
+  },
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+    paths: {
+      sources: "./contracts",
+      tests: "./tests",
+      cache: "./build/cache",
+      artifacts: "./build/artifacts",
+    },
+    mocha: {
+      timeout: 40000,
+    },
+  },
+};
